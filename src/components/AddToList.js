fc8dfd46df9f574763757b12073d5ffb606a2e9e
Bowo <bowo.adej@outlook.com>
2023-12-10 08:32:04 +0000

    add to list package

---
+++ src/components/AddToList.js
@@ -0,0 +1,12 @@
+import { useState, useEffect } from "react";
+import Cookies from 'js-cookie';
+
+export default function AddToList() {
+    return (
+        <button>Add To List</button>
+
+        //Get Cookies of existing words
+        // Append Array with Cookies of Existing Words
+        //Show the list of the existing words
+    )
+}
